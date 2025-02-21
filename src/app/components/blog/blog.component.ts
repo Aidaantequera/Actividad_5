f777553d5f0f71bfb0728964c28af88a84779f1d
Aida_antequera <aidaantequera8@gmail.com>
2025-02-21 21:51:18 +0100

    Añado la funcion getNoticia y le aplico un @if para que si en cada input se añade la informacion me pinte la noticia y sino, que salga una alerta para rellenar todos los campos con el else

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -18,13 +18,13 @@ export class BlogComponent {
   ngOnInit(): void {
   this.noticias = [
     {titulo: 'Perro héroe salva a su dueño de un incendio',
-    imagen: '',
+    imagen: 'https://media.os.fressnapf.com/cms/2024/02/labrador-retriever-liegt-gras.jpg?t=seoimg_937',
     texto: "Un valiente labrador llamado Max se convirtió en un héroe al salvar a su dueño de un incendio en su hogar. El perro ladró sin cesar hasta que su dueño despertó y pudo escapar de las llamas. Gracias a su rápida acción, ambos salieron ilesos.",
     fecha: "20 de febrero de 2025"
   },
 
     {titulo: 'Gato viajero recorre 500 km para volver a casa',
-      imagen: '',
+      imagen: 'https://blog-static.petlove.com.br/wp-content/uploads/2021/01/gato-domestico-pelo-curto-petlove1.jpg',
       texto: "Un gato llamado Oliver sorprendió a todos al recorrer 500 kilómetros para regresar con su familia después de haberse perdido en unas vacaciones. La increíble historia de su regreso demuestra el fuerte vínculo entre las mascotas y sus dueños.",
       fecha: "18 de febrero de 2025"
     }
@@ -33,7 +33,16 @@ export class BlogComponent {
 
   }
    
-  
+  getNoticia(){
+    if (this.nuevaNoticia.titulo && this.nuevaNoticia.imagen && this.nuevaNoticia.texto && this.nuevaNoticia.fecha) {
+      this.noticias.push({ ...this.nuevaNoticia });
+      this.nuevaNoticia = { titulo: "", imagen: "", texto: "", fecha: "" }; // Limpiar formulario
+    } else {
+      alert("Por favor, completa todos los campos antes de publicar.");
+    }
+
+  }
+
 
  
 
